f1e45b4c2122532f5d69453a0ca08e8a44459306
dinuw2002 <thisila100@gmail.com>
2026-04-20 23:47:53 +0530

    docs: add professional README with setup instructions

--- frontend/app/lib/axios.ts
+++ frontend/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:  'https://private-coaching-back-end.onrender.com',
+  baseURL:  'https://private-coaching-back-end.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
